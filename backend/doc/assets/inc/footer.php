805714d4448df8e7b14ffc15efd6d9a84d8df8aa
Durgesh4993 <durgeshKumar4993@gmail.com>
2024-03-02 05:13:50 +0530

    Commmit

---
+++ backend/doc/assets/inc/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                2020 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. Developed By Durgesh Kumar Prajapati</a>
+            </div>
+
+        </div>
+    </div>
+</footer>
---
+++ cons/index.php
@@ -0,0 +1,89 @@
+
+ <!DOCTYPE html>
+<html>
+	<head>
+        <meta http-equiv="refresh" content=""/>	
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Conversation</title>
+        <link rel="stylesheet" href="chatbox/css/bootstrap.min.css"/>
+        <script src="cons/js/jquery_library.js"></script>
+		<script src="cons/js/bootstrap.min.js"></script>
+        <style>
+            body{
+			font-family: "gotham-pro";
+			direction: ltr;
+            }
+            .panel-body{
+                padding-bottom: 10px;
+            }
+            body{
+                background-image: url(https://t3.ftcdn.net/jpg/02/16/47/22/360_F_216472247_cT66WDoS0fp1s3wC7eaykMJNDGVbOBPq.jpg);
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            .navbar-default {
+                background-color: #fff;
+                border-color: #e7e7e7;
+            }
+        </style>
+    </head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top">
+			<div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand"><strong><i class="glyphicon glyphicon-list-alt "></i> Consultancy Services - HMS</strong></a>
+                </div>
+			</div>
+		</nav>
+		<!-- navbar ends-->
+		<div class="container-fluid" style="margin-top:50px;">
+			<br/><div class="row" style="height:600px;">
+				<!-- Main Menu -->
+            <div class="col-sm-3"></div>
+				<div class="col-sm-3">
+                    <div class="panel" style="border-radius:0px; box-shadow:2px 2px 4px 0px">
+                        <div class="panel-heading" id="signin_panel_header">
+                            <p align="center"> <img src="imgs/chat.png" style="height:175px;"/> </p>
+                            <h4 align="center" style="padding-top:5px;">Chat Login</h4><hr/>
+                        </div>
+                        <div class="panel-body" style="margin-top:-30px;">
+                           <center>
+                                  <a class="btn btn-success" style="padding:15px 20px;" href="chatbox/index.php"><i class="glyphicon glyphicon-log-in"></i>  Visit</a>
+                         </center>
+                        </div>
+                    </div>
+                </div>
+                <!-- Admin Menu -->
+				<div class="col-sm-3">
+                    <div class="panel" style="border-radius:0px; box-shadow:2px 2px 4px 0px">
+                        <div class="panel-heading" id="signin_panel_header">
+                            <p align="center"> <img src="imgs/video.png" style="height:175px;"/> </p>
+                            <h4 align="center" style="padding-top:5px;">Video Login</h4><hr/>
+                        </div>
+                        <div class="panel-body" style="margin-top:-30px;">
+                           <center>
+                               <a class="btn btn-success" style="padding:15px 20px;" href="chatbox/admin/"><i class="glyphicon glyphicon-log-in"></i>  Visit</a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            <div class="col-sm-3"></div>
+			</div>
+		</div>
+        <div>
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p style="font-weight: bolder;" align="center">2020 - 2024 © Hospital Management System. Developed By Durgesh Kumar Prajapati</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+	</body>
+</html>
